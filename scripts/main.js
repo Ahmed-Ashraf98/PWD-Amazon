c9be6c76f41ef461e39ac925ab73fec6f665e324
Ahmed Ashraf <a.ashraf.elbendary@gmail.com>
2025-01-02 03:20:34 +0300

    modify mainfest file & add service worker

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,19 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.log("ServiceWorker registration failed: ", error);
+      });
+  });
+}
+
 //================= [APIs] ==========================
 
 const baseUrl = `https://ecommerce.routemisr.com`;
